97dabd644af26537ea1c26ed634fd571c79f83ae
nebula-c <suchoi9709@gmail.com>
2025-06-16 17:56:55 +0900

    Added Nan data for magnet location

--- src/mochi/DataStore.py
+++ src/mochi/DataStore.py
@@ -1,4 +1,5 @@
 import numpy as np
+from mochi.internal_parameter import internal_parameter
 
 class DataStore:
     def __init__(self):
@@ -20,6 +21,7 @@ class DataStore:
         self.__vec_B += inputdata
 
     def get_vec_B(self,):
+        self.blank_magnet()
         return self.__vec_B
     
     def get_vec_r(self,):
@@ -45,5 +47,25 @@ class DataStore:
         self.__lenx = None
         self.__leny = None
         self.__lenz = None
+
+    def blank_magnet(self):
+        if not self.is_data_fine:
+            return
+        
+        for i in range (np.shape(self.__vec_B)[0]):
+            my_vec_B = self.__vec_B[i]
+            my_vec_r = self.__vec_r[i]
+
+            x = my_vec_r[0]
+            y = my_vec_r[1]
+            z = my_vec_r[2]
+
+            if (abs(x) < internal_parameter.x_magnet_len/2) and (abs(y) < internal_parameter.y_magnet_len/2) and (abs(z) < internal_parameter.z_magnet_len/2):
+                self.__vec_B[i][0] = None
+                self.__vec_B[i][1] = None
+                self.__vec_B[i][2] = None
+    
+
+
 
 DataStore = DataStore()
